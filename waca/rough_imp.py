6121ee75b806b24bfdc8eb29f1b186c038ebbffc
jrai51 <87856634+jrai51@users.noreply.github.com>
2022-06-15 00:23:59 -0400

    killed warnings, finished aadp

--- waca/rough_imp.py
+++ waca/rough_imp.py
@@ -2,15 +2,28 @@
 ## IMPORTS ##
 #############
 
-
 import pandas as pd
 import os.path
 import numpy as np
 import matplotlib
 import scipy
 from scipy.fft import fft
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
+from scipy import signal
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+class UserProfile():
+    def __init__(self, userID, start_time, end_time, f_vec):
+        self.userID = userID
+        self.start_time = start_time
+        self.end_time = end_time
+        self.f_vec = f_vec
+    
+    def __str__(self):
+        return self.userID
+
+    def __repr__(self):
+        return str(self.userID) +','+str(self.f_vec)
 
 def waca(user='user1', test='1'):
     ###################
@@ -44,8 +57,8 @@ def waca(user='user1', test='1'):
     accel_df.sort_values('Time', inplace=True)
 
     #data is now separated and ordered by time, ready for M-point filer
-    print(gyro_df.head())
-    print(accel_df.head())
+    #print(gyro_df.head())
+    #print(accel_df.head())
 
     ##################################
     # APPLYING MOVING AVERAGE FILTER #
@@ -54,7 +67,7 @@ def waca(user='user1', test='1'):
     #Applying Moving Average Filter 
 
 
-    M = 10 #M-point filter
+    M = 9 #M-point filter
 
     #starts averaging at Mth point rather than 0th as opposed to vice versa in original paper 
     gyro_df['MA_Time'] = gyro_df['Time'].rolling(M).mean()
@@ -69,7 +82,7 @@ def waca(user='user1', test='1'):
 
     #Creating axis vectors 
 
-    N = 1000 #number of samples for a profile feature 
+    N = 1500 #number of samples for a profile feature 
     M_IDX = N + M -1#index of the Nth sample (accounts for NaNs of first M rows)
 
 
@@ -96,6 +109,7 @@ def waca(user='user1', test='1'):
 
     features = {'x_a': x_a, 'y_a': y_a, 'z_a': z_a, 'x_g': x_g, 'y_g': y_g, 'z_g': z_g}
     features_df = pd.DataFrame.from_dict(features) #mostly for presentation purposes, will come in handy for feature extraction
+    
 
     #the below may be necessary calculations (averaged time start/end)
     #start_time = gyro_df.MA_Time.loc[gyro_df.MA_Time.first_valid_index()]
@@ -128,11 +142,29 @@ def waca(user='user1', test='1'):
         base = 2
         return -(vc * np.log(vc)/np.log(base)).sum()
 
+    def aadp(X):
+        ''' Calculates average absolute difference of peaks of a list X'''
+
+        #calculates the difference between each adjacent pair
+        peaks = scipy.signal.find_peaks(X)
+        peaks = list(peaks[0])
+        try:
+            sum = 0
+            n = 0 #number of differences calculated 
+            for i in range(1, len(peaks)):
+                sum += abs(peaks[i]-peaks[i-1])
+                n += 1
+
+            return sum/ n
+        except:
+            return 0
+
     extracted_features = { 'mean': [], 'median': [], 'variance': [], 'AADP': [], 'range': [], 'mode':[], 
                     'covariance': [], 'mad': [], 'iqr': [], 'correlation': [], 'skewness': [], 'kurtosis': [],
                     'entropy': [], 's_nrg': []} #features in the domain of frequency
 
     for column in features_df:
+        extracted_features['AADP'].append(aadp(features_df[column].tolist()))
         extracted_features['mean'].append(features_df[column].mean())
         extracted_features['median'].append(features_df[column].median())
         extracted_features['variance'].append(features_df.var()[column])
@@ -142,15 +174,13 @@ def waca(user='user1', test='1'):
         extracted_features['skewness'].append(features_df[column].skew())
         extracted_features['kurtosis'].append(features_df[column].kurtosis())
         extracted_features['mad'].append(features_df[column].mad()) 
+
         #calculate the FFT for next calculations
         col_fft = fft(features_df[column].to_numpy())
         extracted_features['entropy'].append(shannon_entropy(column))  
         extracted_features['s_nrg'].append(spectral_energy(col_fft)) #what is up with the +0.00j??? 
-
-
-        #need fixing :
-        extracted_features['AADP'].append(0) ######################FIX
-        
+        #extracted_features['AADP'].append(0)
+    
 
     labels = ['x_a', 'y_a', 'z_a', 'x_g', 'y_g', 'z_g']
 
@@ -171,18 +201,15 @@ def waca(user='user1', test='1'):
 
     feature_set = pd.DataFrame.from_dict(extracted_features, orient='index', columns=labels)
 
-    print(feature_set)
 
     user_id = PARTICIPANT
     t_start = gyro_df.Time.loc[gyro_df.Time.first_valid_index()]
     t_end = gyro_df.Time.iloc[-1]
     f_vec = feature_set.unstack().to_frame().sort_index(level=1).T
     f_vec.columns = f_vec.columns.map('_'.join)
-    pp.pprint('F_VEC: '+str(f_vec.iloc[0].tolist()))
     normal_vec = normalize(np.array(f_vec.iloc[0].tolist()))
 
-    user_profile = [user_id, t_start, t_end, normal_vec]
-    #print(user_profile)
+    user_profile = UserProfile(user_id, t_start, t_end, normal_vec)
     
     return user_profile
 
@@ -197,16 +224,37 @@ def normalize(V):
     return normal_vec 
 
 
-users = [] #For now, this array works as the 'database' to store user profiles to test against each other 
+users = {'test1': [], 'test2': [], 'test3': []} #For now, this array works as the 'database' to store user profiles to test against each other 
 
+def get_users():
+    directory = "C:\\Users\\Jagrit Rai\\Documents\\GitHub\\Wearable_Continuous_Authentication\\waca\\user_data"
+    
+    for root, subdirectories, files in os.walk(directory):
+        for subdirectory in subdirectories:
+            try:
+                users['test1'].append(waca(subdirectory, 1))
+                users['test2'].append(waca(subdirectory, 2))
+                users['test3'].append(waca(subdirectory, 3))
+            except:
+                pass
+  
+
+get_users()
+
+#database = pd.DataFrame.from_dict(users)
+print(len(users['test1']))
+print(len(users['test2']))
+print(len(users['test3']))
+
+###########
+# MAKE IT INTO A DATAFRAME
+##########
 
 user_1 = waca('user1', 1)
-user_2 = waca('user2', 1)
 user_1_2 = waca('user1', 2)
+user_2 = waca('user2', 1)
 
-users.append(user_1)
-users.append(user_2)
-users.append(user_1_2)
+
 
 ###################
 # DECISION MODULE #
@@ -214,7 +262,6 @@ users.append(user_1_2)
 
 threat_threshold = 0.05
 
-print()
 
 def minkow_dist(x, y, p=2):
     #takes two vectors stored as lists to return minkowski distance. 
@@ -226,17 +273,15 @@ def minkow_dist(x, y, p=2):
     return distance_sum ** (1/p)
 
 
-pp.pprint('LIST:'+str( user_1[3]))
-
-
 dist = minkow_dist(user_1[3], user_1_2[3], 2)
 
-print('DISTANCES:\n', dist)
-#manhattan_dist = scipy.cityblock()
+manhattan_dist = scipy.spatial.distance.cityblock(user_1.f_vec, user_1_2.f_vec)
 
-print()
 
-if dist < threat_threshold:
+
+print('DISTANCES:\n', manhattan_dist)
+
+if manhattan_dist < threat_threshold:
     print('VERIFIED')
 else:
     print('UNVERIFIED')
